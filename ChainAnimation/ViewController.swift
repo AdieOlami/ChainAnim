e84b7e784f2d928880a2feb70c3936284fca9223
Adie Olalekan <ugbeadie@gmail.com>
2019-03-23 16:33:23 +0100

    Initial Commit

---
+++ ChainAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChainAnimation
+//
+//  Created by Olar's Mac on 3/23/19.
+//  Copyright © 2019 Adie Olami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
